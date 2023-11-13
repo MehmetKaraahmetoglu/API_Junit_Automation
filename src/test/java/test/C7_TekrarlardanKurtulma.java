214d3b45bce0501159cab22891b9a29fef4d4793
MehmetKaraahmetoglu <mek_memin@hotmail.com>
2023-11-13 19:40:08 +0100

    API lessons are added.

---
+++ src/test/java/test/C7_TekrarlardanKurtulma.java
@@ -0,0 +1,44 @@
+package test;
+
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class C7_TekrarlardanKurtulma {
+      /*
+    https://restful-booker.herokuapp.com/booking/10 url’ine bir GET request gonderdigimizde donen Response’un,
+            status code’unun 200,
+            ve content type’inin application/json, ve response body’sindeki
+            "firstname"in, “Susan”,
+            ve "lastname"in, “Brown”,
+            ve "totalprice"in, 390,
+            ve "depositpaid"in, false,
+	        ve "additionalneeds"in, “Breakfast” oldugunu test edin
+     */
+
+    @Test
+    public void nonRepeat(){
+        // 1- Endpoint hazırla
+
+        String url="https://restful-booker.herokuapp.com/booking/10";
+
+        // 2- Expected Body verilmediği için hazırlanmadı
+
+
+        // 3- Response kaydet
+        Response response=given().when().get(url);
+
+
+        //4- Assertion yap
+
+        response.then().assertThat().statusCode(200).contentType("application/json")
+                .body("firstname", equalTo("Mark"),
+                        "lastname", equalTo("Brown"),
+                        "totalprice", equalTo(390),
+                        "depositpaid", equalTo(false),
+                        "additionalneeds", equalTo("Breakfast"));
+    }
+}
